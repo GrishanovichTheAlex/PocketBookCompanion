95280e94a0a29927d2a8b8737ac52cb5fe1071f8
Alex <incrudwork@gmail.com>
2022-06-07 22:40:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+Assembly.
+
+Console.WriteLine("Please, specify the path to the root folder:");
+
+var rootDirectory = Console.ReadLine();
+
+var entities = Directory.GetFileSystemEntries(rootDirectory, "*",
+    enumerationOptions: new EnumerationOptions { MaxRecursionDepth = 2, RecurseSubdirectories = true });
+
+Console.WriteLine(entities.Count());
+
+foreach (var entity in entities)
+    Console.WriteLine(entity);
+
+Console.ReadKey();
